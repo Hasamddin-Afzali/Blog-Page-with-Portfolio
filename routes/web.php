3c43db6283322fe313130ded82ead559f67b1e0e
Hasamuddin Afzali <66642113+Hasamddin-Afzali@users.noreply.github.com>
2024-02-14 12:11:21 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers;
 Route::get('/admin/frogotPass', 'ForgottenPasswordGetController@frogotPass')->name("frogotPass");
 //Route::get('/admin/allUsers', 'AllUsersGetController@allUsers')->name("allUsers");
 Route::get('/admin/profile', 'ProfileGetController@profile')->name("profile");
-Route::get('/admin/projects', 'ProjectsGetController@projects')->name("projects");
 Route::get('/admin/categories', 'CategoriesGetController@categories')->name("categories");
 
 Route::post('upload_image', 'UploadController@upload')->name('upload');
@@ -18,6 +17,7 @@ Route::get('blog/category', 'Controller@blogByCategory')->name('category');
 Route::get('blog/{id}', 'Controller@post')->name('singlePost');
 Route::get('portfolio', 'Controller@portfolio')->name('portfolio');
 Route::get('contact', 'Controller@contact')->name('contact');
+Route::post('send-contact', 'ContactController@getMessage')->name('getContactMessage');
 
 Route::prefix('admin')->name('admin.')->group(function (){
     Route::get('login', 'UserController@loginPage')->name('loginPage');
@@ -34,6 +34,10 @@ Route::prefix('admin')->name('admin.')->group(function (){
         Route::get('users', 'UserController@allUsers')->name("allUsers");
         
         Route::get('category', 'CategoryController@category')->name('category');
+        Route::get('projects', 'ProjectsGetController@projects')->name("projects");
+        Route::post('projects/add-new-project', 'ProjectsGetController@addNewProject')->name('addNewProject');
+        Route::post('projects/edit-project', 'ProjectsGetController@editProject')->name('editProject');
+        Route::post('projects/delete-project', 'ProjectsGetController@deleteProject')->name('deleteProject');
         Route::get('contacts', 'ContactController@contacts')->name('contacts');
         Route::get('logout', 'UserController@logout')->name('logout');
     });
