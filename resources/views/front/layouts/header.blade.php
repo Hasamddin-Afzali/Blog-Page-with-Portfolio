25de38ce104d6368392c63bbf130beadff284ea5
Hasamuddin Afzali <hasamuddin.afz@gmail.com>
2024-03-09 20:11:17 +0300

    New Features

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -18,7 +18,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-black">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="{{asset('/img/logo.png')}}" alt="" style="width:150px;"></a>
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{asset('/img/logo.png')}}" alt="" style="width:150px;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
