54faecf99da4c6434c5f603cc2fcae030ca523bd
Hasamuddin Afzali <hasamuddin.afz@gmail.com>
2024-03-09 20:06:24 +0300

    New Features

--- resources/views/front/singlePage.blade.php
+++ resources/views/front/singlePage.blade.php
@@ -1,39 +1,49 @@
 @extends('front.layouts.master')
 @section('title', 'Blog Post')
 @section('content')
+<style>
+    .overlay {
+    position: absolute;
+    inset: 0;
+    background-color: rgba(0, 0, 0, 0.8); /* Adjust the opacity as needed */
+}
 
-<header class="bg-cover bg-center h-screen d-flex align-items-center text-white bg-no-repeat position-relative"
-    style="width:100%;height:300px;background-image: url('{{$post[0]->img_path}}');">
-    <div class="position-absolute inset-0 bg-black bg-opacity-50"></div>
+</style>
+<header class="bg-cover bg-center h-screen d-flex align-items-center text-white bg-no-repeat position-relative" style="width:100%;height:300px;background-image: url('{{$post[0]->img_path}}');">
+    <div class="overlay"></div>
     <div class="container mx-auto text-center position-relative z-index-10">
-        <h1 class="text-5xl font-bold mb-4"><span class="text-white">{{$post[0]->title}}</span></h1>
+        <h1 class="text-5xl font-bold mb-4"><span class="text-warning">{{$post[0]->title}}</span></h1>
     </div>
 </header>
+
+
 <div class="container mx-auto">
-    <div class="py-5 d-flex justify-content-center align-items-start">
-        <!-- Left Column -->
-        <div class="w-75 me-5">
-            <!-- Blog Title -->
-            <div class="title mb-4">
-                <h1 class="text-3xl font-bold">{{$post[0]->title}}</h1>
+    <div class="py-5">
+        <div class="row">
+            <!-- Left Column -->
+            <div class="col-12 col-md-8">
+                <!-- Blog Title -->
+                <div class="title mb-4">
+                    <h1 class="text-3xl font-bold">{{$post[0]->title}}</h1>
+                </div>
+                <!-- Blog Date -->
+                <div class="date mb-4 text-gray-500">
+                    {{$post[0]->created_at->format('F d, Y')}} | {{$post[0]->getRelation('category')->title}}
+                </div>
+                <!-- Blog Description -->
+                <div class="description">
+                    <p>{!! $post[0]->body !!}</p>
+                </div>
             </div>
-            <!-- Blog Date -->
-            <div class="date mb-4 text-gray-500">
-                {{$post[0]->created_at->format('F d, Y')}} | {{$post[0]->getRelation('category')->title}}
-            </div>
-            <!-- Blog Description -->
-            <div class="description">
-                <p>{!! $post[0]->body !!}</p>
-            </div>
-        </div>
-        <!-- Right Column -->
-        <div class="w-25">
-            <div class="bg-white p-4 shadow-lg">
-                <h3 class="text-xl font-bold mb-4">Related Blogs</h3>
-                <hr>
-                <ul class="my-4">
-                    {{\App\Http\Controllers\Controller::listBlogLinksByCategory($post[0]->getRelation('category')->id, $post[0]->id)}}
-                </ul>
+            <!-- Right Column -->
+            <div class="col-12 col-md-4 mt-4 mt-md-0">
+                <div class="bg-white p-4 shadow-lg">
+                    <h3 class="text-xl font-bold mb-4">Related Blogs</h3>
+                    <hr>
+                    <ul class="my-4">
+                        {{\App\Http\Controllers\Controller::listBlogLinksByCategory($post[0]->getRelation('category')->id, $post[0]->id)}}
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
